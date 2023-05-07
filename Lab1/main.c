70742786e60eccc45a35ef960c0843afa52666d0
fwics88 <fwics88@gmail.com>
2023-05-07 19:47:03 +0900

    lab1

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,4 +1,31 @@
+#include <assert.h>
+
+#include "lab1.h"
+
 int main(void)
 {
+    {
+        int num = 0;
+
+        num = add(1, 2);
+        assert(num == 3);
+
+        num = add(1, -2);
+        assert(num == -1);
+    }
+
+    {
+        int num = 0;
+
+        num = sub(2, 1);
+        assert(num == 1);
+
+        num = sub(2, -1);
+        assert(num == 3);
+
+        num = sub(1, 2);
+        assert(num == -1);
+    }
+
     return 0;
 }
