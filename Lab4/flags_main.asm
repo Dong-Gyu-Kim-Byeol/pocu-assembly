0904b00f97aec012823da86f6f4a4f32e3b5dcc3
fwics88 <fwics88@gmail.com>
2023-05-29 03:08:48 +0900

    lab4

--- Lab4/flags_main.asm
+++ Lab4/flags_main.asm
@@ -3,13 +3,232 @@
 
     .INCLUDE "utils/utils.asm"
 
-    ;===DEFINE RESET OUT MEM HERE===
-    ; 0xFFFFFFFF
-    lda #$FF
-    sta $2300
-    sta $2301
-    sta $2302
-    sta $2303
+    ;----------AllClear----------
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #$00
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $00
+    assert $2302, $00
+    assert $2303, $00
+    ;============================
+    ;----------AllClear----------
+
+    ;---------NegativeSet--------
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #$80
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $01
+    assert $2301, $00
+    assert $2302, $00
+    assert $2303, $00
+    ;============================
+    ;---------NegativeSet--------
+
+    ;---------OverflowSet--------
+    ;===DEFINE INPUT HERE===
+    clc ; OverflowSet
+    lda #$40
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $01
+    assert $2302, $00
+    assert $2303, $00
+    ;============================
+    ;---------OverflowSet--------
+
+    ;-----------ZeroSet----------
+    ;===DEFINE INPUT HERE===
+    clc ; ZeroSet
+    lda #$02
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $00
+    assert $2302, $01
+    assert $2303, $00
+    ;============================
+    ;-----------ZeroSet----------
+
+    ;---------CarrySet-----------
+    ;===DEFINE INPUT HERE===
+    clc ; CarrySet
+    lda #$01
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $00
+    assert $2302, $00
+    assert $2303, $01
+    ;============================
+    ;---------CarrySet-----------
+
+    ;--------TwoFlagsSet---------
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%00000011
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $00
+    assert $2302, $01
+    assert $2303, $01
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%10000001
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $01
+    assert $2301, $00
+    assert $2302, $00
+    assert $2303, $01
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%10000010
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $01
+    assert $2301, $00
+    assert $2302, $01
+    assert $2303, $00
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%11000000
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $01
+    assert $2301, $01
+    assert $2302, $00
+    assert $2303, $00
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%01000001
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $01
+    assert $2302, $00
+    assert $2303, $01
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%01000010
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $01
+    assert $2302, $01
+    assert $2303, $00
+    ;============================
+    ;--------TwoFlagsSet---------
+
+    ;-------ThreeFlagsSet--------
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%10000011
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $01
+    assert $2301, $00
+    assert $2302, $01
+    assert $2303, $01
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%01000011
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $00
+    assert $2301, $01
+    assert $2302, $01
+    assert $2303, $01
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%11000010
+    pha
+    php
     ;========================
 
     .INCLUDE "flags.asm"
@@ -18,8 +237,25 @@
     assert $2300, $01
     assert $2301, $01
     assert $2302, $01
+    assert $2303, $00
+    ;============================
+
+    ;===DEFINE INPUT HERE===
+    clc
+    lda #%11000001
+    pha
+    php
+    ;========================
+
+    .INCLUDE "flags.asm"
+
+    ;===DEFINE ASSERTIONS HERE===
+    assert $2300, $01
+    assert $2301, $01
+    assert $2302, $00
     assert $2303, $01
     ;============================
+    ;-------ThreeFlagsSet--------
 
     termin
 
